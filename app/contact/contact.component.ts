e670074dbf14ef8c5a4eb32f5bb5e6b3d8454eea
Theo <theo@diro.co.za>
2020-02-14 14:14:36 +0200

    Sending Email

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -4,18 +4,42 @@ import { RouterExtensions } from 'nativescript-angular/router';
 import 'rxjs/add/operator/switchMap';
 import { DrawerPage } from '~/shared/drawer/drawer.page';
 
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import * as Email from 'nativescript-email';
+
 @Component({
   selector: 'app-contactus',
-    moduleId: module.id,
+  moduleId: module.id,
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactUsComponent extends DrawerPage {
   errMess: string;
 
-  constructor(private routerExtensions: RouterExtensions, 
-              private changeDetectorRef:ChangeDetectorRef) 
-  { 
-        super(changeDetectorRef);
+  constructor(private routerExtensions: RouterExtensions,
+    private changeDetectorRef: ChangeDetectorRef,
+    private fonticon: TNSFontIconService
+  ) {
+    super(changeDetectorRef);
   }
+
+
+  sendEmail() {
+
+    Email.available()
+      .then((avail: boolean) => {
+        if (avail) {
+          Email.compose({
+            to: ['confusion@food.net'],
+            subject: '[ConFusion]: Query',
+            body: 'Dear Sir/Madam:'
+          });
+        }
+        else
+          console.log('No Email Configured');
+      })
+
+  }
+
+
 } 
